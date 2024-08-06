2e3c6ebaf3be6d9414cf412a409c819de12134fd
Milad Joodi <winbeta.com@gmail.com>
2024-08-07 01:55:32 +0330

    Start 🎁🎉

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client"
+import Image from 'next/image'
+import { useEffect } from 'react';
+import GlobalApi from './_utils/GlobalApi';
+import { useUser } from '@clerk/nextjs';
+
+export default function Home() {
+  
+  return (
+  <div>
+  
+  </div>
+  )
+}
