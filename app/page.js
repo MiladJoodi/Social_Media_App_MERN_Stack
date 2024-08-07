2f81ac38b0004c76122acad23fd0717ab9a17952
Milad Joodi <winbeta.com@gmail.com>
2024-08-07 19:24:52 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@
 import Image from 'next/image'
 import { useEffect } from 'react';
 import GlobalApi from './_utils/GlobalApi';
-import { useUser } from '@clerk/nextjs';
+import { UserButton, useUser } from '@clerk/nextjs';
 
 export default function Home() {
   
   return (
   <div>
-  
+    <UserButton />
   </div>
   )
 }
