39189b82da3da371f70f3f8b395b85e43a5db4a8
Milad Joodi <winbeta.com@gmail.com>
2024-08-07 20:00:56 +0330

    profile

--- app/(routes)/home/_components/Banner.js
+++ app/(routes)/home/_components/Banner.js
@@ -1,22 +1,27 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Banner() {
   return (
-    <div className='p-2 bg-white-400 
+    <div
+      className="p-2 bg-white-400 
     rounded-xl shadow-sm flex items-center
     bg-blue-100
-     border gap-5'>
-        <Image src='/social-panda-1.png' width={200} height={200} alt='panda'/>
-        <div>
-        <h2 className='font-bold text-[29px] '>WELCOME TO SOCIAL PANDA</h2>
-        <h2 className=''>Join Community, Create and Share your thought</h2>
-        <Button className="mt-3 bg-blue-500">Get Started</Button>
-        </div>
-        
+     border gap-5"
+    >
+      <Image src="/social-panda-1.png" width={200} height={200} alt="panda" />
+      <div>
+        <h2 className="font-bold text-[29px] ">WELCOME TO SOCIAL PANDA</h2>
+        <h2 className="">Join Community, Create and Share your thought</h2>
+        <Link href="/sign-up">
+          <Button className="mt-3 bg-blue-500">Get Started</Button>
+        </Link>
+        {" "}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
